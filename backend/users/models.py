0663ef47db154966d8715ffcb86e512a9017c9d1
Созыкин Родион <skykanade@gmail.com>
2024-12-19 23:46:49 +0500

    set mixins to organization view

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,23 +3,22 @@ from django.db import models
 
 
 class User(AbstractUser):
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
-
     email = models.EmailField(max_length=254, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username", "first_name", "last_name")
 
 
 class Subscription(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='follower',
-        verbose_name='Пользователь',
+        related_name="follower",
+        verbose_name="Пользователь",
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='following',
-        verbose_name='Подписки',
+        related_name="following",
+        verbose_name="Подписки",
     )
